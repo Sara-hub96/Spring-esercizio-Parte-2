6ec02d1d5eaab5f249b70a69e5ae2f67d4175b6d
Sara-hub96 <138792116+Sara-hub96@users.noreply.github.com>
2023-11-13 22:15:40 +0100

    Update CalendarRepository.java

--- src/main/java/co/develhope/springcalendar/repository/CalendarRepository.java
+++ src/main/java/co/develhope/springcalendar/repository/CalendarRepository.java
@@ -3,5 +3,6 @@ package co.develhope.springcalendar.repository;
 import co.develhope.springcalendar.model.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 }
