b7028d51ac5aab14e5f6abc47aab175b56b57b5e
Sara-hub96 <138792116+Sara-hub96@users.noreply.github.com>
2023-11-13 22:16:03 +0100

    Update EventsRepository.java

--- src/main/java/co/develhope/springcalendar/repository/EventsRepository.java
+++ src/main/java/co/develhope/springcalendar/repository/EventsRepository.java
@@ -3,5 +3,6 @@ package co.develhope.springcalendar.repository;
 import co.develhope.springcalendar.model.Events;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface EventsRepository extends JpaRepository<Events, Long> {
 }
